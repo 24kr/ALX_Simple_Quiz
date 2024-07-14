805caddb144039c479447ac548531ec157cb8ba5
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-07-14 20:37:19 +0200

    Settled all functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// Addition function
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -7,3 +8,37 @@ function add(number1, number2) {
     const result = add(number1, number2);
     document.getElementById('calculation-result').textContent = result;
   });
+
+//  Subtraction function
+function sub(number1, number2) {
+    return number1 - number2;
+}
+document.getElementById('subtract').addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = sub(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+// Multipliction 
+function mult(number1, number2) {
+    return number1 * number2;
+}
+document.getElementById('multiply').addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = mult(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+// Divition function
+function div(number1, number2) {
+    return number1 / number2;
+}
+document.getElementById('divide').addEventListener('click', function () {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = div(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
