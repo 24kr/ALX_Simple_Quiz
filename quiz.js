0ad9e77ee1fdc9d9af20dd32f740ca1418ff336b
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-07-14 23:57:07 +0200

    added missing part

--- quiz.js
+++ quiz.js
@@ -1,16 +1,17 @@
 function checkAnswer() {
     var correctAnswer = 4;
-    var userAnswer = document.getElementsByName('quiz');
+    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    if(userAnswer){
 
-    for (i = 0; i < userAnswer.length; i++) {
-        if (userAnswer[i].checked = correctAnswer)
+        if (userAnswer.value === correctAnswer)
         {
-            window.alert('Correct! Well done.');
+            document.getElementById("feedback").innerHTML ="Correct! Well done."
+            // window.alert('Correct! Well done.');
         } 
         else{
-            window.alert('Thats incorrect.Try again!');
-        } 
-        }
+            document.getElementById("result").innerHTML ="That's incorrect.Try again!"
+            // window.alert('Thats incorrect.Try again!');
+        } }
 
     }
 document.getElementById('submit-answer').addEventListener('click', function () {
