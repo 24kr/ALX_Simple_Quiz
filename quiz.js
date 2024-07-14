8f2c30c5c45a4638d51375bc76f72a51db9d53a6
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-07-14 23:22:03 +0200

    Modified function

--- quiz.js
+++ quiz.js
@@ -1,5 +1,17 @@
 function checkAnswer() {
-    // Function body
     var correctAnswer = 4;
-    var userAnswer;
-}
+    var userAnswer = document.getElementsByName('quiz');
+
+    for (i = 0; i < userAnswer.length; i++) {
+        if (userAnswer[i].checked = correctAnswer)
+        {
+            window.alert('Correct! Well done.');
+        } 
+        else{
+            window.alert('Thats incorrect.Try again!');
+        } 
+        }
+
+    }
+document.getElementById('submit-answer').addEventListener('click', function () {
+    checkAnswer();})
